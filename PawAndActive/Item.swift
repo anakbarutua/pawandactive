ace128dab99e8ab42b7b95ac92bd6cd3f6a05918
Kelvin Ananda <ananda.kelvin@gmail.com>
2024-08-06 19:10:47 +0700

    Initial Commit

---
+++ PawAndActive/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  PawAndActive
+//
+//  Created by Kelvin Ananda on 06/08/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
